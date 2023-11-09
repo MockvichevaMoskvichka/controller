2601ac789162037cc8bb3de9da4daa4695c24720
MockvichevaMoskvichka <150232782+MockvichevaMoskvichka@users.noreply.github.com>
2023-11-09 15:22:35 +0400

    Add files via upload

---
+++ choose a number/src/com/itproger/Main.java
@@ -0,0 +1,35 @@
+package  com.itproger;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Напишите свое желание");
+        String str  = scanner.nextLine();
+        System.out.println("напишите цифру от 1 до 5");
+        int num = scanner.nextInt();
+switch (num){
+   case 1:
+       System.out.println("Ваше желание сбудется через 1 день");
+       break;
+    case 2:
+        System.out.println("Ваше желание сбудется через 2 день");
+        break;
+    case 3:
+        System.out.println("Ваше желание сбудется через 3 день");
+        break;
+    case 4:
+        System.out.println("Ваше желание сбудется через 4 день");
+        break;
+    case 5:
+        System.out.println("Ваше желание сбудется через 5 день");
+        break;
+    default:
+        System.out.println("ОШИБКА!!Введите цифры от 1 до 5");
+}
+    }}
+
+
+
